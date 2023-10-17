13220c0ee0c97bca2997f19f622e88e45fbcb481
StephanieOrantes2024 <stephanie.orantes2024@hilasal.superate.org.sv>
2023-10-16 20:44:00 -0600

    Personajes

---
+++ src/Personajes.js
@@ -0,0 +1,22 @@
+class Personaje {
+    constructor(nombre, clase, nivel, experiencia) {
+        this.nombre = nombre;
+        this.clase = clase;
+        this.nivel = nivel;
+        this.experiencia = experiencia;
+    }
+
+    atacar(enemigo) {
+        // Lógica de combate
+        console.log(`${this.nombre} ataca a ${enemigo.nombre}`);
+    }
+
+    ganarExperiencia(experienciaGanada) {
+        this.experiencia += experienciaGanada;
+        // Lógica para subir de nivel si es necesario
+        if (this.experiencia >= this.nivel * 100) {
+            this.nivel++;
+            console.log(`${this.nombre} sube de nivel a ${this.nivel}`);
+        }
+    }
+}
